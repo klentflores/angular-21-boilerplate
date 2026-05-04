91e160dff376d16f129f165087f31789a992109d
klentflores <klentflores25@gmail.com>
2026-05-04 17:16:45 +0800

    3rdddddddddddd committt!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

--- auth-app/src/app/profile/layout.component.ts
+++
@@ -1,4 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({ selector: 'app-profile-layout', templateUrl: 'Layout.component.html', standalone: false })
-export class LayoutComponent { }
