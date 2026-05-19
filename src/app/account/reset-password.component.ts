729dd8e9bebceb1afe3ac63f97cd0a8ae27a25eb
klentflores <klentflores25@gmail.com>
2026-05-19 09:57:45 +0800

    commiittteeeddddddddddddddd!!!!!!!!!!!!

--- src/app/account/reset-password.component.ts
+++ src/app/account/reset-password.component.ts
@@ -1,8 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { first } from 'rxjs/operators';
-
 import { AccountService, AlertService } from '@app/_services';
 import { MustMatch } from '@app/_helpers';
 
@@ -26,44 +25,54 @@ export class ResetPasswordComponent implements OnInit {
         private route: ActivatedRoute,
         private router: Router,
         private accountService: AccountService,
-        private alertService: AlertService
+        private alertService: AlertService,
+        private cdr: ChangeDetectorRef,
     ) { }
 
     ngOnInit() {
         this.form = this.formBuilder.group({
             password: ['', [Validators.required, Validators.minLength(6)]],
-            confirmPassword: ['', Validators.required],
+            confirmPassword: ['', Validators.required]
         }, {
-            validators: MustMatch('password', 'confirmPassword')
+            validator: MustMatch('password', 'confirmPassword')
         });
 
         const token = this.route.snapshot.queryParams['token'];
 
-        // ✅ Validate FIRST, only clear URL after success
+        // remove token from url to prevent http referer leakage
+        this.router.navigate([], { relativeTo: this.route, replaceUrl: true });
+
         this.accountService.validateResetToken(token)
             .pipe(first())
             .subscribe({
                 next: () => {
                     this.token = token;
                     this.tokenStatus = TokenStatus.Valid;
-                    this.router.navigate([], { relativeTo: this.route, replaceUrl: true });
+                     this.cdr.detectChanges();
                 },
                 error: () => {
                     this.tokenStatus = TokenStatus.Invalid;
+                    this.cdr.detectChanges();
                 }
             });
     }
 
+    // convenience getter for easy access to form fields
     get f() { return this.form.controls; }
 
     onSubmit() {
         this.submitted = true;
+
+        // reset alerts on submit
         this.alertService.clear();
 
-        if (this.form.invalid) return;
+        // stop here if form is invalid
+        if (this.form.invalid) {
+            return;
+        }
 
         this.loading = true;
-        this.accountService.resetPassword(this.token!, this.f['password'].value, this.f['confirmPassword'].value)
+        this.accountService.resetPassword(this.token!, this.f.password.value, this.f.confirmPassword.value)
             .pipe(first())
             .subscribe({
                 next: () => {
